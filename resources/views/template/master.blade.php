0c67ab9b6c06bee147f2174341f0f6817f09d501
CEP <galcera@hotmail.com>
2019-02-22 13:24:02 +0100

    Update master - mainpage y creacion views

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -12,7 +12,44 @@
 </head>
 <body>
 
-    {{-- navbar --}}
+    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary p-3">
+        <a class="navbar-brand" href="/frutes_ToniA/public">
+            PETLINKS
+        </a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+        </div>
+
+         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
+        <form class="form-inline my-2 my-lg-0">
+          <a class="bg-transparent border-0 my-2 my-sm boton pr-4" href="register.php">Regístrate</a>
+          <a class="btn btn-primary my-2 my-sm-0 boton boton-login" href="login.php">Iniciar Sesión</a>
+        </form>
+      </div>
+    </nav>
+
+     <nav class="navbar navbar-expand-lg navbar-light bg-primary p-0">
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0  title-font">
+          <li class="nav-item">
+            <a class="nav-link text-white ml-2 mr-2 pl-3" href="index.php">HOME
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white ml-2 mr-2 pl-3" href="gamesGrid.php">ABOUT US</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white ml-2 mr-2  pl-3" href="index.php#scroll">DONATE</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white ml-2 mr-2  pl-3" href="contact.php">RRSS</a>
+          </li>
+
+        </ul>
+      </div>
+    </nav>
 
     <div class="container">@yield('contenidor')</div>
 
