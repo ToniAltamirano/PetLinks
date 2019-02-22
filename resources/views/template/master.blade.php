b6976ac6e4fbbcf074a5de06d17ad1c37ff89eae
CEP <galcera@hotmail.com>
2019-02-22 12:34:45 +0100

    .

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="icon" href="{{ url('img/groceries.ico') }}" type="image/x-icon">
     <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
     <script src="{{ asset('js/popper.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
