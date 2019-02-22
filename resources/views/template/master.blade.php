2e0e3991e424f01d2da7c72a21e1dd0b283cc833
CEP <galcera@hotmail.com>
2019-02-22 12:29:24 +0100

    Carpeta - archivo master.blade.php

---
+++ resources/views/template/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="icon" href="{{ url('img/groceries.ico') }}" type="image/x-icon">
+    <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <title>@yield('titulo')</title>
+</head>
+<body>
+
+{{-- navbar --}}
+
+    <div class="container">@yield('contenidor')</div>
+
+</body>
+</html>
