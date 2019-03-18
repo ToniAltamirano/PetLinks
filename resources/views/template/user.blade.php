e6b598ad7ea3edb20bea5c1c94dfa3f3cf07ae2f
Francesc Retamal <franrere.6.97@gmail.com>
2019-03-18 23:12:45 +0100

    Update user.blade.php

--- resources/views/template/user.blade.php
+++ resources/views/template/user.blade.php
@@ -5,8 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
         <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
         @yield('own_CSS')
         <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
