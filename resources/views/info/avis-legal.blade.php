52c167648cfe1cec43dcb91e99e8fab2db6e4120
Adrià Martínez Mayné <97.martimez@gmail.com>
2019-03-24 18:18:23 +0100

    Avís Legal Form
    Primera versión del formulario de aviso legal.

--- resources/views/info/avis-legal.blade.php
+++ resources/views/info/avis-legal.blade.php
@@ -3,5 +3,57 @@
 @section('titulo', 'Avís legal')
 
 @section('contenidor')
-
+<div class="container">
+        <div class="row">
+            <div class="card my-5">
+                <div class="card-body col-md-10 text-justify mx-auto">
+                    <h1 class="mb-3 text-center card-title">Avís Legal</h1>
+                    <p>En compliment de la Llei 34/2002, d'11 de juliol, de Serveis de la Societat de la Informació i de Comerç electrònic, s’informa a l’Usuari que el titular de la pàgina web www.protectoramataro.org/ és de la SOCIETAT PROTECTORA D'ANIMALS DE MATARÓ (en endavant SPAM), i les seves dades d’identificació són les següents:</p>
+                    <p>Domicili social: Ctra. Nacional II, Km. 648, 4<br>
+                        CP: 08302<br>
+                        Població: Mataró<br>
+                        Província: Barcelona<br>
+                        NIF: G58223785<br>
+                        Correu electrònic: info@protectoramataro.org</p>
+                    <ol>
+                        <li><strong>ACCEDIR A LA PÀGINA WEB:</strong></li>
+                        <p>L’Avís Legal regula l’accés i ús dels Usuaris a la pàgina web i té per objectiu donar a conèixer els serveis de la protectora i permetre l'accés general de tots els usuaris d'Internet.</p>
+                        <p>L'accés i/o ús de la Web atribueix a qui ho realitza la condició d'Usuari i l'acceptació, sense reserves de cap classe, de totes i cadascuna de les presents condicions generals, així com d'aquelles altres particulars que, si escau, regeixin la utilització del Portal o dels serveis vinculats al mateix.</p>
+                        <p>L'usuari ha de llegir atentament l’Avís Legal i les Polítiques de Privacitat i de Cookies quan es proposi utilitzar la Pàgina Web, ja que SPAM es reserva la facultat de realitzar, en qualsevol moment i sense necessitat de cap preavís, qualsevol modificació o actualització dels continguts i serveis, de les presents disposicions d'accés i ús i, en general, de quants elements integrin el disseny i configuració del seu Web. Si no accepta les condicions d’accés i ús, li preguem s'abstingui d'utilitzar el Web i el seu contingut.</p>
+                        <li><strong>UTILITZACIÓ DE LA PÀGINA WEB:</strong></li>
+                        <p>L'Usuari es compromet a fer un ús diligent de la Pàgina Web, així com de la informació relativa als seus serveis i/o activitats, amb total subjecció tant a la normativa aplicable, així com amb la moral i els bons costums generalment acceptades i l'ordre públic, les condicions d’accés i ús i qualssevol altres condicions establertes a la Pàgina Web.</p>
+                        <p>A més, es compromet a abstenir-se d'utilitzar qualsevol dels continguts amb finalitats o efectes il·lícits, prohibits en el present text, lesius dels drets i interessos de tercers, o que de qualsevol forma puguin danyar, inutilitzar, sobrecarregar, deteriorar o impedir la normal utilització dels continguts, d'altres Usuaris o de qualsevol usuari d'Internet (maquinari i programari).</p>
+                        <li><strong>FUNCIONAMENT DE LA PÀGINA WEB:</strong></li>
+                        <p>En cas d’incompliment de les condicions de l’Avís Legal, o les Polítiques de Privacitat i Cookies, SPAM es reserva el dret a limitar, suspendre i/o excloure l’accés a la seva Pàgina Web, adoptant qualsevol mesura tècnica necessària al seu respecte. SPAM farà el possible per mantenir la pàgina Web en bon funcionament, evitant errors, o bé reparant-los i mantenint els continguts actualitzats Ara bé, SPAM no garanteix la disponibilitat i continuïtat en l’accés a la Pàgina Web ni la inexistència d’errors en el contingut.</p>
+                        <li><strong>RESPONSABILITAT:</strong></li>
+                        <p>L’Usuari és l’únic responsable de l’ús que es pugui fer de qualsevol informació o mecanisme de la Pàgina Web.</p>
+                        <p>SPAM no es farà responsable de cap dany en el hardware i/o software de l’Usuari que es derivi de l’accés i ús de la Pàgina Web. Així mateix, tampoc es responsabilitzarà d’aquests danys i/o perjudicis que es puguin ocasionar per l’accés i/o ús de la informació de la Pàgina Web, i en concret dels que es puguin produir en sistemes informàtics o els provocats per virus /o atacs informàtics, caigudes, interrupcions absència o defecte en les comunicacions i/o Internet.</p>
+                        <p>L’Usuari serà responsable dels danys i/o perjudicis que SPAM pugui patir com a conseqüència de l’incompliment de qualsevol de els obligacions a les que queda sotmès a través del present Avís Legal, la normativa aplicable i Política de Privacitat i de Cookies.</p>
+                        <li><strong>POLÍTICA EN MATÈRIA DE “LINKS” (WEB ENLLAÇANT I WEB ENLLAÇADA):</strong></li>
+                        <ul>
+                            <li><em><strong>Web enllaçant:</strong></em></li>
+                            <p>Els tercers que tinguin la intenció d’incloure en una pàgina web un enllaç de la present pàgina web hauran de complir amb la legislació vigent i no podran allotjar continguts que siguin inapropiats, il·lícits, pornogràfics, violents, etc.</p>
+                            <p>En cap cas SPAM es fa responsable del contingut de la Pàgina Web, ni promou, garanteix, supervisa ni recomana els continguts d’aquesta.</p>
+                            <p>Si la Web enllaçant incompleix algun del aspectes anteriors estarà obligat a suprimir l’enllaç de manera immediata.</p>
+                        </ul>
+                        <ul>
+                            <li><em><strong>Web enllaçada:</strong></em></li>
+                            <p>En aquesta Pàgina Web es poden incloure enllaços de pàgines web de tercers que permetin a l’Usuari accedir-hi. No obstant, SPAM no es fa responsable del contingut d’aquestes pàgines web enllaçades, sinó que serà l’Usuari l’encarregat d’acceptar i comprovar els accessos cada vegada que hi accedeixi. Tals enllaços o esments tenen una finalitat que no impliquen el suport, l'aprovació, comercialització o relació alguna entre la pàgina i les persones o entitats titulars dels llocs on es trobin.</p>
+                        </ul>
+                        <li><strong>PROPIETAT INTEL·LECTUAL I INDUSTRIAL SOBRE ELS CONTINGUTS:</strong></li>
+                        <p>SPAM o els seus llicenciants, són titulars de tots els drets de propietat intel·lectual sobre els Continguts de la Pàgina Web, entenent com a tal tots els dissenys, bases de dades, programes d’ordinadors subjacents (codi font, inclòs), així com els diferents elements que integren la Pàgina Web (textos, gràfics, fotografies, vídeos, colors, etc.), estructura, ordre, etc. En relació a les marques i noms comercials (“signes distintius”) són titularitat de SPAM o els llicenciants.</p>
+                        <p>L’ús de la Pàgina Web per part de l’Usuari no suposa la cessió de cap dret de propietat intel·lectual ni industrial. L’Usuari té totalment prohibit de reproduir, copiar, distribuir, posar a disposició o de qualsevol altra forma de comunicar públicament, transformar o modificar els Continguts o els Signes distintius, tret que es compti amb l'autorització del titular dels corresponents drets o això resulti legalment permès.</p>
+                        <li><strong>PUBLICITAT:</strong></li>
+                        <p>A la pàgina Web podran allotjar-se continguts publicitaris o patrocinats. Els anunciants o patrocinadors són els únics responsables d'assegurar-se que el material remès per a la seva inclusió en la Web compleix amb les lleis que en cada cas puguin ser aplicable.</p>
+                        <p>SPAM no serà responsable de cap error, inexactitud o irregularitat que puguin contenir els continguts publicitaris o dels patrocinadors.</p>
+                        <li><strong>LEGISLACIÓ APLICABLE:</strong></li>
+                        <p>L’Avís Legal es regirà i interpretarà de conformitat amb la legislació espanyola.</p>
+                        <p>Per la resolució de qualsevol conflicte que pogués derivar-se de l’accés al lloc web, es sotmetran als jutjats o tribunals pertinents de conformitat amb la normativa de consumidors i usuaris.</p>
+                        <li><strong>CONTACTE:</strong></li>
+                        <p>Per qualsevol pregunta o comentari sobre el present Avís Legal us podeu posar en contacte amb nosaltres a través del correu electrònic info@protectoramataro.org.</p>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
