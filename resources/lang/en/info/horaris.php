1ab8f94c3654a78c15e79a60ab4f278a7f362904
Adrià Martínez Mayné <97.martimez@gmail.com>
2019-03-26 17:27:29 +0100

    Horaris Lang
    Implementados los idiomas en el formulario de horarios.

---
+++ resources/lang/en/info/horaris.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | HORARIS Language Lines (EN - English)
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'title' => 'Schedule',
+    'offices_title' => 'Offices schedule:',
+    'offices_text' => 'From Monday to Friday from 9 a.m. to 7 p.m.',
+    'veterinary_title' => 'Veterinary Space schedule (Mataró):',
+    'veterinary_text' => 'From Monday to Saturday, from 11 a.m. to 2 p.m. and from 3 p.m. to 6 p.m. (adoptions from 11 a.m. to 1 p.m. and from 3 p.m. to 5 p.m.).',
+    'veterinary_text_2' => 'Sundays and holidays, from 11 a.m. to 2 p.m. (adoptions from 11 a.m. to 1 p.m.).',
+    'ccaac_title' => 'Cal Pilé (Mataró) and the CCAAC of Barcelonès schedule (Badalona):',
+    'ccaac_text' => 'From Monday to Friday, from 11 a.m. to 2:30 p.m. and from 3 p.m. to 5 p.m.',
+    'ccaac_text_2' => 'Saturdays, from 11 a.m. to 2:30 p.m. and from 3 p.m. to 6 p.m.',
+    'ccaac_text_3' => 'Sundays and holidays, from 11 a.m. to 2 p.m.',
+    'moret_title' => 'Can Moret schedule (L\'Ametlla del Vallès):',
+    'moret_text' => 'From Monday to Thursday, from 11 a.m. to 1 p.m.',
+    'moret_text_2' => 'Friday, from 11 a.m. to 2:30 p.m. and from 3 p.m. to 4 p.m.',
+    'moret_text_3' => 'Saturdays, from 11 a.m. to 2:30 p.m. and from 3 p.m. to 6 p.m.',
+    'moret_text_4' => 'Sundays and holidays, from 11 a.m. to 2 p.m.',
+    'important' => 'IMPORTANT:',
+    'important_text' => 'If you come to adopt, try to arrive at least half an hour before closing, in the case of refuges, and one hour before closing in the case of the veterinary space.',
+];
--- resources/views/info/horaris.blade.php
+++ resources/views/info/horaris.blade.php
@@ -8,12 +8,32 @@
             <div class="col-sm-10 col-md-8 col-lg-8 mx-auto">
                 <div class="card card-signin my-5">
                     <div class="card-body text-justify">
-                        <h1 class="mb-3 text-center card-title">Horaris</h1>
-                        <p><strong>Horaris d'oficines:</strong><br>De dilluns a divendres de 9 a 19h.<br></p>
-                        <p><strong>Horaris Espai Veterinari (Mataró):</strong><br>De dilluns a dissabtes, d'11 a 14h i de 15 a 18h (per adopcions d'11 a 13h i de 15 a 17h).<br>Diumenges i festius, d'11 a 14h (per adopcions, d'11 a 13h).<br></p>
-                        <p><strong>Horaris Cal Pilé (Mataró) i del CCAAC del Barcelonès (Badalona):</strong><br>De dilluns a divendres, d'11 a 14'30h i de 15 a 17h.<br>Dissabtes, d'11 a 14'30h i de 15 a 18h.<br>Diumenges i festius, d'11 a 14h.<br></p>
-                        <p><strong>Horaris de Can Moret (L'Ametlla del Vallès):</strong><br>De dilluns a dijous, d'11 a 13h. Divendres, d'11 a 14'30 i de 15 a 16h.<br>Dissabtes, d'11 a 14'30 i de 15 a 18h.<br>Diumenges i festius, d'11 a 14h.</p>
-                        <p><strong>IMPORTANT:</strong> Si veniu per adoptar, procureu arribar al menys mitja hora abans de tancar, en el cas dels refugis, i una hora abans de tancar en el cas de l'espai veterinari</p>
+                        <h1 class="mb-3 text-center card-title">{{ __('info/horaris.title') }}</h1>
+                        <p>
+                            <strong>{{ __('info/horaris.offices_title') }}</strong>
+                            <br>{{ __('info/horaris.offices_text') }}<br>
+                        </p>
+                        <p>
+                            <strong>{{ __('info/horaris.veterinary_title') }}</strong>
+                            <br>{{ __('info/horaris.veterinary_text') }}
+                            <br>{{ __('info/horaris.veterinary_text_2') }}<br>
+                        </p>
+                        <p>
+                            <strong>{{ __('info/horaris.ccaac_title') }}</strong>
+                            <br>{{ __('info/horaris.ccaac_text') }}
+                            <br>{{ __('info/horaris.ccaac_text_2') }}
+                            <br>{{ __('info/horaris.ccaac_text_3') }}<br>
+                        </p>
+                        <p>
+                            <strong>{{ __('info/horaris.moret_title') }}</strong>
+                            <br>{{ __('info/horaris.moret_text') }}
+                            <br>{{ __('info/horaris.moret_text_2') }}
+                            <br>{{ __('info/horaris.moret_text_3') }}
+                        </p>
+                        <p>
+                            <strong>{{ __('info/horaris.important') }}</strong>
+                            {{ __('info/horaris.important_text') }}
+                        </p>
                     </div>
                 </div>
             </div>
