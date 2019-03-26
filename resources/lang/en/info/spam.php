ea8e538d672807b0742bd02c86a392eb955eba79
AdriaMayne <97.martimez@gmail.com>
2019-03-26 11:06:36 +0100

    SPAM Form Lang
    Añadidos los idiomas al archivo info/spam.

---
+++ resources/lang/en/info/spam.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | SPAM Language Lines (EN - English)
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'title' => 'Who we are and what we do',
+    'name' => 'Animal Welfare Association of Mataró (SPAM)',
+    'objective' => 'We have been working since 1971 with the aim of welcoming, protecting and promoting the adoption of abandoned animals. We manage shelters with a protectionist philosophy, we fight for the life of the animals and we work day after day to avoid their suffering. Our task is also informative: we raise awareness in society about responsible ownership of pets, about the rights of animals, about the benefits of adoption and about a good coexistence between animals and citizens.',
+    'main_tasks' => 'Main activities',
+    'task_1_title' => 'Collection and reception of lost and abandoned animals',
+    'task_1_text' => 'In the reception centers we manage, we offer welfare and veterinary attention to the animals until they are adopted.',
+    'task_2_title' => 'Management of colonies of stray cats',
+    'task_2_text' => 'We sterilize, feed and take care of the hygiene and sanitary control of the stray cats of the colonies that we control.',
+    'task_3_title' => 'Promotion of adoption',
+    'task_3_text' => 'That the animals are adopted is our priority. We work to disseminate and make known the animals of the shelters, and we help the animals that have it more difficult through specific campaigns that facilitate their adoption.',
+    'task_4_title' => 'Sensitization',
+    'task_4_text' => 'We bring to the public the reality of the abandonment of pets and the need to have them in a responsible manner.',
+    'directors_board' => 'Board of Directors',
+    'president' => 'Chairwoman',
+    'treasurer' => 'Treasurer',
+    'secretary' => 'Secretary',
+    'vicepresident' => 'Vice President',
+    'members' => 'Members',
+];
--- resources/views/info/contacta.blade.php
+++ resources/views/info/contacta.blade.php
@@ -8,26 +8,26 @@
             <div class="col-sm-10 col-md-8 col-lg-8 mx-auto">
                 <div class="card card-signin my-5">
                     <div class="card-body">
-                        <h1 class="h3 mb-3 text-center font-weight-normal card-title">{{ __('contacto.title') }}</h1>
+                        <h1 class="h3 mb-3 text-center font-weight-normal card-title">{{ __('info/contacto.title') }}</h1>
                         <form class="form-signin" action="" method="post">
                             @csrf
                             <!-- Email -->
                             <div class="form-group">
-                                <label for="email">{{ __('contacto.email') }}</label>
-                                <input type="email" id="email" class="form-control" name="email" placeholder="{{ __('contacto.email_placeholder') }}" value="{{ old('email') }}" required autofocus>
+                                <label for="email">{{ __('info/contacto.email') }}</label>
+                                <input type="email" id="email" class="form-control" name="email" placeholder="{{ __('info/contacto.email_placeholder') }}" value="{{ old('email') }}" required autofocus>
                             </div>
                             <!-- Nombre -->
                             <div class="form-group">
-                                <label for="nombre">{{ __('contacto.name') }}</label>
-                                <input type="text" id="nombre" class="form-control" name="nombre" placeholder="{{ __('contacto.name_placeholder') }}" value="{{ old('email') }}" required>
+                                <label for="nombre">{{ __('info/contacto.name') }}</label>
+                                <input type="text" id="nombre" class="form-control" name="nombre" placeholder="{{ __('info/contacto.name_placeholder') }}" value="{{ old('email') }}" required>
                             </div>
                             <!-- Missatge -->
                             <div class="form-group">
-                                <label for="nombre">{{ __('contacto.message') }}</label>
-                                <textarea id="nombre" class="form-control" name="nombre" placeholder="{{ __('contacto.message_placeholder') }}" value="{{ old('email') }}" rows="5" required></textarea>
+                                <label for="nombre">{{ __('info/contacto.message') }}</label>
+                                <textarea id="nombre" class="form-control" name="nombre" placeholder="{{ __('info/contacto.message_placeholder') }}" value="{{ old('email') }}" rows="5" required></textarea>
                             </div>
                             <!-- Submit -->
-                            <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">{{ __('contacto.contacto_btn') }}</button>
+                            <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">{{ __('info/contacto.contacto_btn') }}</button>
                         </form>
                     </div>
                 </div>
