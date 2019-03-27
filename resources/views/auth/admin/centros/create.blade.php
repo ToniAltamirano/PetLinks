60b3a2c65c1d4456736ccd1fe24c63c837866532
Arnau Infante <arnauinfantepinos@gmail.com>
2019-03-27 11:11:01 +0100

    some updates

--- resources/views/auth/admin/centros/create.blade.php
+++ resources/views/auth/admin/centros/create.blade.php
@@ -6,7 +6,27 @@
             <h5 class="card-title">NUEVO CENTRO</h5>
         </div>
         <div class="card-body">
+            <form action="{{ action('/centros') }}" method="POST">
+                @csrf
+                <div class="row">
+                    <div class="form-group row col-12 col-md-6">
+                        <label class="form-control col-md-2" for="nombre">{{ __("admin/centros.name"}})</label>
+                        <input type="text" name="nombre" id="txtNombre" class="form-control col-md-4">
+                    </div>
+                    <div class="form-group row col-12 col-md-6">
 
+                    </div>
+                    <div class="form-group row col-12 col-md-6">
+
+                    </div>
+                    <div class="form-group row col-12 col-md-6">
+
+                    </div>
+                    <div class="form-group row col-12 col-md-6">
+
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
