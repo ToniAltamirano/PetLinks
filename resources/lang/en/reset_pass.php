a7b460b7354830bbdcee6cee1544eaf5c11b9aff
Arnau Infante <arnauinfantepinos@gmail.com>
2019-04-04 21:05:57 +0200

    reset pass form email field in multilang

--- resources/lang/en/reset_pass.php
+++ resources/lang/en/reset_pass.php
@@ -10,6 +10,7 @@ return [
     */
 
     'title' => 'Reset password',
+    'email' => 'Insert your email',
     'desc'=> 'Create a new password to recover your account.',
     'password' => 'New password',
     'password_repeat' => 'Confirm tour new password',
