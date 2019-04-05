8c628366a6c9271577764bea4bd4df4c3b5f64a8
Toni <mansoksama@gmail.com>
2019-04-05 11:42:21 +0200

    Traduccion usuarios completa

--- resources/lang/en/admin/usuarios.php
+++ resources/lang/en/admin/usuarios.php
@@ -9,10 +9,29 @@ return [
     |
     */
 
-    '' => '',
-    '' => '',
-    '' => '',
+    'title' => 'Users',
 
+    'nombre' => 'Name',
+    'placeholder_nombre' => 'Introduce name',
+
+    'apellidos' => 'Surnames',
+    'placeholder_apellidos' => 'Introduce surnames',
+
+    'nombreUsuario' => 'Username',
+    'placeholder_nombreUsuario' => 'Introduce username',
+
+    'email' => 'Email',
+    'placeholder_email' => 'Introduce email ',
+
+    'password' => 'Password',
+    'placeholder_password' => 'Introduce the password',
+
+    'repeatPassword' => 'Repeat password',
+    'placeholder_repeatPassword' => 'Repeat the password',
+
+    'rol' => 'Rol',
+    'volver' => 'Back',
+    'crearUsuari' => 'Create',
 
     'destroy_success_message' => 'Users correctly deleted.',
     'update_success_message' => 'Users correctly updated.',
