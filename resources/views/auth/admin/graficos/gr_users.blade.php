d5a6f55054374e0d6ecbde4b40c83414268fc9de
Adrià Martínez Mayné <97.martimez@gmail.com>
2019-04-07 13:12:26 +0200

    User/Donantes JS
    Creadas las carpetas y archivos correspondientes para los JS de los gráficos de Usuarios y Donantes.

--- resources/views/auth/admin/graficos/gr_users.blade.php
+++ resources/views/auth/admin/graficos/gr_users.blade.php
@@ -7,7 +7,7 @@
                 <div class="card-title">
                     <h3>Rol</h3>
                 </div>
-                <div id="pieChartRoles" class="text-center"></div>
+                <div id="pieChartRoles" class="text-center" style="width: 100%; height: 500px;"></div>
             </div>
         </div>
     </div>
@@ -15,57 +15,5 @@
 
 @section('scripts')
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-        function graficoTiposDonacion(){
-            $('#pieChartRoles').html('<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>');
-            var tipos_grafico = [];
-            $.ajax({
-                type: "GET",
-                url: "../api/usuario/roles",
-                dataType: "json",
-                async: 'true',
-                success: function(json) {
-                    //array de subtipos de donaciones de las donaciones
-                    var donantes_roles = json.data.donantes_roles;
-                    //array de tipos de donaciones de la bd
-                    var roles = json.data.roles;
-
-                    //array donde meteremos la informacion que necesitemos
-                    var tipos_char = [];
-                    roles.forEach(rol => {
-                        tipos_char.push({"id": rol.id, "rol" : rol.rol, "cantidad" : 0});
-                    });
-
-                    donantes_roles.forEach(donante_rol => {
-                        tipos_char.forEach(tipo_char => {
-                            if (donante_rol.roles_id == tipo_char.id) {
-                                tipo_char.cantidad += 1;
-                            }
-                        });
-                    });
-
-                    //array que le pasaremos al charts para crear el grafico
-                    var tipos_grafico = [["Rol", "Cantidad"]];
-                    tipos_char.forEach(tipo_char => {
-                        tipos_grafico.push([tipo_char.rol, tipo_char.cantidad]);
-                    });
-                    $('#pieChartRoles').html('');
-                    google.charts.load('current', {'packages':['corechart']});
-                    google.charts.setOnLoadCallback(drawChart);
-
-                    function drawChart() {
-                        var data = google.visualization.arrayToDataTable(tipos_grafico);
-                        var options = {
-
-                        };
-
-                        var chart = new google.visualization.PieChart(document.getElementById('pieChartRoles'));
-                        chart.draw(data, options);
-                    }
-                }
-            });
-        }
-
-        graficoTiposDonacion();
-    </script>
+    <script src="{{ asset('js/events/graphics/users/rol_Users_Donut.js') }}"></script>
 @endsection
