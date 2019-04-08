d256df4f3b83fbfc098cde0249f8c6c21aa01d99
AdriaMayne <97.martimez@gmail.com>
2019-04-08 12:31:39 +0200

    Horaris Rework

--- resources/views/info/horaris.blade.php
+++ resources/views/info/horaris.blade.php
@@ -5,7 +5,7 @@
 @section('contenidor')
 <div class="container">
         <div class="row">
-            <div class="col-sm-10 col-md-8 col-lg-8 mx-auto">
+            <div class="col-lg-10 mx-auto">
                 <div class="card card-signin my-5">
                     <div class="card-body text-justify">
                         <h1 class="mb-3 text-center card-title">{{ __('info/horaris.title') }}</h1>
@@ -31,7 +31,7 @@
                             <br>{{ __('info/horaris.moret_text_3') }}
                         </p>
                         <p>
-                            <strong>{{ __('info/horaris.important') }}</strong>
+                            <strong>{{ __('info/horaris.important') }}</strong><br>
                             {{ __('info/horaris.important_text') }}
                         </p>
                     </div>
