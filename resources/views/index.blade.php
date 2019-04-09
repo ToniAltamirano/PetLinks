e3d7ebca26c266285fa2afef0e57acb0b0ae399b
Adrià Martínez Mayné <97.martimez@gmail.com>
2019-04-09 20:16:55 +0200

    Prueba Index Page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,19 @@
 @section('titulo', 'PETLINKS')
 
 @section('contenidor')
+    @foreach($campaigns as $key=>$campaign)
+        <div class="card">
+            <img src="{{ asset('storage/'. $campaign->imagen) }}" class="card-img" alt="...">
+            <div class="card-img-overlay">
+                <div class="card bg-caption d-inline-block p-3">
+                    <h1 class="">{{ $campaign->titulo_ca }}</h1>
+                    <h4 class="">{{ $campaign->subtitulo_ca }}</h3>
+                </div>
+            </div>
+        </div>
+    @endforeach
 
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    {{-- <div id="myCarousel" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators" sytle="z-index: -1;">
             @for($i=0; $i< count($campaigns);$i++)
                 @if($i==0)
@@ -56,20 +67,7 @@
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
-</div>
-
- {{--  <div class="container">
-        <div class="row">
-          <div class="col-8">
-            <h3 class="pt-5">VISITA NUESTRA PROTECTORA!</h3>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              <button class="btn btn-primary" target="_blank" onclick="window.location.href='https://www.protectoramataro.org/es/necesitamos-donaciones'"> DONATE !</button>
-      </div>
-          <div class="col-4">
-            <img class="p-5"src="{{ asset('img/img.jpg') }}" alt=" ERROR">
-          </div>
-        </div>
-        </div>  --}}
+</div> --}}
 @endsection
 
 @section('scripts')
