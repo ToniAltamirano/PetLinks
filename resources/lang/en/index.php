b936a593ba402a3f02ebbafe6f1fff7bd2006c95
Adrià Martínez Mayné <97.martimez@gmail.com>
2019-04-10 23:08:04 +0200

    Index Text Lang

--- resources/lang/en/index.php
+++ resources/lang/en/index.php
@@ -9,6 +9,7 @@ return [
     |
     */
 
-    'presentation' => '',
+    'presentation' => 'We are a society that offers a welcome service for those pets that are lost or that have been abandoned.',
+    'presentation2' => 'Our objectives are to ensure the welfare of the animals during the time of stay in the center, providing adequate facilities, food and veterinary assistance, promote responsible adoptions of those animals not recovered by the owners.',
     'campaigns' => 'Campaigns',
 ];
--- resources/lang/es/index.php
+++ resources/lang/es/index.php
@@ -9,6 +9,7 @@ return [
     |
     */
 
-    'presentation' => '',
+    'presentation' => 'Somos una sociedad que ofrece servicio de acogida para aquellos animales domésticos que están perdidos o que han sido abandonados.',
+    'presentation2' => 'Nuestros objetivos son garantizar el bienestar de los animales durante el tiempo de estancia en el centro, proporcionando unas instalaciones, una alimentación y una asistencia veterinaria adecuadas, impulsar las adopciones responsables de aquellos animales no recuperados por los propietarios.',
     'campaigns' => 'Campañas',
 ];
