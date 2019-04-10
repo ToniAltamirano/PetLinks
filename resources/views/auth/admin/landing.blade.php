51c21cbf1a0b25913a12c3b28728225cffab9f3d
Arnau Infante <arnauinfantepinos@gmail.com>
2019-04-10 22:46:24 +0200

    retoques landing

--- resources/views/auth/admin/landing.blade.php
+++ resources/views/auth/admin/landing.blade.php
@@ -8,13 +8,13 @@
 
 @section('datos')
 
-<div class="container">
+<div class="container mt-5">
     <div id="general_menu" class="row">
         <div class="col-lg-6 mt-3">
             <a href="{{ url('/donaciones/create') }}">
                 <div class="card mb-3 card-landing">
                     <div class="card-body card-landing">
-                        <h2>{{ __('admin/landing.add_donation') }}</h2>
+                        <h2 class="mt-md-3 display-4">{{ __('admin/landing.add_donation') }}</h2>
                         <i class="material-icons md-48 big">euro_symbol</i>
                     </div>
                 </div>
@@ -24,7 +24,7 @@
             <a href="{{ url('/donantes/create') }}">
                 <div class="card mb-3 card-landing">
                     <div class="card-body card-landing">
-                        <h2>{{ __('admin/landing.add_donner') }}</h2>
+                        <h2 class="mt-3 display-4" class="mt-3 display-4">{{ __('admin/landing.add_donner') }}</h2>
                         <i class="material-icons md-48 big">add</i>
                     </div>
                 </div>
@@ -34,7 +34,7 @@
             <a href="{{ url('/usuarios/create') }}">
                 <div class="card mb-3 card-landing">
                     <div class="card-body card-landing">
-                        <h2>{{ __('admin/landing.add_user') }}</h2>
+                        <h2 class="mt-3 display-4" class="mt-3 display-4">{{ __('admin/landing.add_user') }}</h2>
                         <i class="material-icons md-48 big">person_add</i>
                     </div>
                 </div>
@@ -44,7 +44,7 @@
             <a onclick="showStatsMenu()">
                 <div class="card mb-3 card-landing">
                     <div class="card-body card-landing">
-                        <h2>{{ __('admin/landing.see_stats') }}</h2>
+                        <h2 class="mt-3 display-4" class="mt-3 display-4">{{ __('admin/landing.see_stats') }}</h2>
                         <i class="material-icons md-48 big">show_chart</i>
                     </div>
                 </div>
@@ -56,7 +56,7 @@
             <a href="{{ url('/graficos/donaciones') }}">
                 <div class="card mb-3 card-landing">
                     <div class="card-body card-landing">
-                        <h2>{{ __('admin/landing.donations') }}</h2>
+                        <h2 class="mt-3 display-4">{{ __('admin/landing.donations') }}</h2>
                         <i class="material-icons md-48 big">euro_symbol</i>
                     </div>
                 </div>
@@ -66,7 +66,7 @@
             <a href="{{ url('/graficos/usuarios') }}">
                 <div class="card mb-3 card-landing">
                     <div class="card-body card-landing">
-                        <h2>{{ __('admin/landing.users') }}</h2>
+                        <h2 class="mt-3 display-4">{{ __('admin/landing.users') }}</h2>
                         <i class="material-icons md-48 big">person_outline</i>
                     </div>
                 </div>
@@ -76,7 +76,7 @@
             <a href="{{ url('/graficos/donantes') }}">
                 <div class="card mb-3 card-landing">
                     <div class="card-body card-landing">
-                        <h2>{{ __('admin/landing.donners') }}</h2>
+                        <h2 class="mt-3 display-4">{{ __('admin/landing.donners') }}</h2>
                         <i class="material-icons md-48 big">person</i>
                     </div>
                 </div>
@@ -86,7 +86,7 @@
             <a href="{{ url('/graficos/centros') }}">
                 <div class="card mb-3 card-landing">
                     <div class="card-body card-landing">
-                        <h2>{{ __('admin.stats_centers') }}</h2>
+                        <h2 class="mt-3 display-4">{{ __('admin.stats_centers') }}</h2>
                         <i class="material-icons md-48 big">domain</i>
                     </div>
                 </div>
